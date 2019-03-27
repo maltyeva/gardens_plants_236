5114e15cbb0ba6e3b061cd6f165e0694c1ae24db
Maria <maria.altyeva@gmail.com>
2019-03-27 18:29:35 +0800

    deleted cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :gardens do
-    resources :plants, only: [:new, :create, :destroy, :index]
+    resources :plants, only: [:new, :create, :index]
   end
+
+  resources :plants, only: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
